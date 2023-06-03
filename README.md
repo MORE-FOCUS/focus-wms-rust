f28feb608f2268c96135ab0f5807b37744cd7b85
more <874048298@qq.com>
2023-06-04 01:13:04 +0800

    初始化项目

---
+++ README.md
@@ -0,0 +1,7 @@
+# MORE-ADMIN-RUST
+#### RUST实现应用后台管理系统
+
+#### 使用组件
+1.  actix-web
+2.  rbatis
+
---
+++ src/main.rs
@@ -0,0 +1,7 @@
+fn main()-> std::io::Result<()>{
+    start_server()
+}
+
+fn start_server()->std::io::Result<()>{
+    Ok(())
+}
